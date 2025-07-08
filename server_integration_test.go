155537754f1304319edba8f7a7fef00f398de286
Frank Jahn <10794823+fjahn78@users.noreply.github.com>
2025-07-08 15:09:00 +0200

    replace inmemstore in integration tests with file store

--- server_integration_test.go
+++ server_integration_test.go
@@ -7,7 +7,10 @@ import (
 )
 
 func TestRecordingWinsAndRetrievingThem(t *testing.T) {
-	server := NewPlayerServer(NewInMemoryPlayerStore())
+	database, cleanDatabase := createTempFile(t, "")
+	defer cleanDatabase()
+	store := &FileSystemPlayerStore{database: database}
+	server := NewPlayerServer(store)
 	player := "Pepper"
 
 	server.ServeHTTP(httptest.NewRecorder(), newPostWinRequest(player))
