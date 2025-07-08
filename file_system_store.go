a0a27b676e412afd55a3a5925685badb40e41e0d
Frank Jahn <10794823+fjahn78@users.noreply.github.com>
2025-07-08 14:36:45 +0200

    GREEN

--- file_system_store.go
+++ file_system_store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"encoding/json"
 )
 
 type FileSystemPlayerStore struct {
@@ -9,7 +10,16 @@ type FileSystemPlayerStore struct {
 }
 
 func (f *FileSystemPlayerStore) RecordWin(name string) {
-	
+	league := f.GetLeague()
+
+	for i, player := range league {
+		if player.Name == name {
+			league[i].Wins++
+			break
+		}
+	}
+	f.database.Seek(0, io.SeekStart)
+	json.NewEncoder(f.database).Encode(league)
 }
 
 func (f *FileSystemPlayerStore) GetPlayerScore(name string) int {
